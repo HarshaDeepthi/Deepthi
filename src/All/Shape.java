95b136f5370dcea197d4cd46048d4d936b9054ce
CodexBox <harshamulisetti56@gmail.com>
2022-05-18 15:39:49 +0530

    commit harsha

--- src/All/Shape.java
+++ src/All/Shape.java
@@ -8,27 +8,5 @@ public  interface Shape {
     public double area();
 
 }
-class Rectangle implements Shape {
-    int l;
-    int w;
-     Rectangle(int l,int w){
-         this.l=l;
-         this.w=w;
-     }
-     public void draw(){
-        System.out.println("Rectangle has been draw");
-    }
-    public  double area(){
-         return l*w;
-    }
- }
- class Test{
-    public static void main(String Args[]){
-
-        Rectangle rect=new Rectangle(10,20);
-        System.out.println("area of rectangle= " +rect.area());
-        rect.draw();
-        }
-    }
 
 
---
+++ src/Immutable.java
@@ -0,0 +1,7 @@
+public class Immutable {
+    public static void main(String[] args){
+        Employee e=new Employee("HFDGFH12345");
+        String S1=e.getAdharnumber();
+        System.out.println("adharNumber=" +S1);
+    }
+}
